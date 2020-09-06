f8b9702d803328955aa537c44fc53f4300b2c98e
Luciano Pedesol <60025358+lucianopedesol@users.noreply.github.com>
2020-09-06 13:45:50 -0300

    Update estatistica.py

--- estatistica.py
+++ estatistica.py
@@ -1,33 +1,31 @@
-# importing functools for reduce()
 import functools
-
-# importing operator for operator functions
 import operator
 
-
 print(
-    "Gerador de questões COMBINATÓRIAS, onde a ordem das escolhas não importa. Do tipo:"
+    "\n\nGerador de questões COMBINATÓRIAS, onde a ordem das escolhas não importa. Do tipo:"
 )
 
 print(
-    '\n\n Uma estante de biblioteca tem {qtd_livros} livros: {qtd_livro_EF} exemplares o livro "Combinatória é fácil" e {qtd_livro_NED} exemplares de "Combinatória não é difícil". Considere que os livros com mesmo título sejam indistinguíveis. Determine de quantas maneiras diferentes podemos dispor os {qtd_livros} livros na estante de modo que dois exemplares de "Combinatória não é difícil" nunca estejam juntos.'
+    '\n\nUma estante de biblioteca tem {qtd_livros} livros: {qtd_livro_EF} exemplares do livro "Combinatória é fácil" \n'
+    'e {qtd_livro_NED} exemplares de "Combinatória não é difícil". Considere que os livros com mesmo título sejam \n'
+    'indistinguíveis. Determine de quantas maneiras diferentes podemos dispor os {qtd_livros} livros na estante de modo que \n'
+    'dois exemplares de "Combinatória não é difícil" nunca estejam juntos.'
 )
-
 
 quest = 1
-qt = 1
 
-d = True
-while (d):
+continuar = True
+while continuar:
 
-    print("\n\n Insira os dados abaixo:")
+    print("\n\nInsira os dados abaixo:")
 
     n = input(
         "\n\nInsira o valor da quantidade de livros, de modelo determinado, que tem o maior volume: {qtd_livro_EF} \n"
     )
 
     k = input(
-        "Insira o valor da quantidade de livros, de modelo determinado, que não pode ficar um do lado do outro: {qtd_livro_NED}\n"
+        "Insira o valor da quantidade de livros, de modelo determinado, que não pode ficar "
+        "um do lado do outro: {qtd_livro_NED}\n"
     )
 
     if not k.isnumeric() or not n.isnumeric():
@@ -38,68 +36,71 @@ while (d):
     else:
         n = int(n)
         k = int(k)
-        kk = k
-        nn = n + 1
+
         if n < k or k == 0:
             print(
                 "\n\n***Não é possivel satisfazer o calculo, tente novamento com novos valores.***\n\n"
             )
             continue
 
-    qt = qt - 1
-    quest = quest - (qt)
+
+
     print(f" \n\nQuestão ({quest})")
     print(
-        f'Uma estante de biblioteca tem {(nn + kk)-1} livros: {nn-1} exemplares o livro "Combinatória é fácil" e {kk} exemplares de "Combinatória não é difícil". Considere que os livros com mesmo título sejam indistinguíveis. Determine de quantas maneiras diferentes podemos dispor os {(nn + kk)-1} livros na estante de modo que dois exemplares de "Combinatória não é difícil" nunca estejam juntos.'
+        f'Uma estante de biblioteca tem {(n + k)} livros: {n} exemplares o livro "Combinatória é fácil" '
+        f'\ne {k} exemplares de "Combinatória não é difícil". Considere que os livros com mesmo título sejam indistinguíveis. '
+        f'\nDetermine de quantas maneiras diferentes podemos dispor os {(n + k)} livros na estante '
+        f'\nde modo que dois exemplares de "Combinatória não é difícil" nunca estejam juntos.'
     )
     print("\nAguarde os valores serem gerados para calculo.\n")
-    quest += 1
-    qt += 1
 
-    dif = nn - k
+    quest += 1
+
+    n = n + 1
+    dif = n - k
     total_dif = list(range(1, dif + 1))
 
-    dif_fat = str(total_dif).replace(",", "*")
-    dif_fat = "".join(dif_fat)
-    proddif = functools.reduce(operator.mul, total_dif)
+    dif_fatorial = str(total_dif).replace(",", "*")
+    dif_fatorial = "".join(dif_fatorial)
+    dif_produto = functools.reduce(operator.mul, total_dif)
 
-    total_n = list(range(1, nn + 1))
+    total_n = list(range(1, n + 1))
 
-    n_fat = str(total_n).replace(",", "*")
-    n_fat = "".join(n_fat)
-    n = functools.reduce(operator.mul, total_n)
+    n_fatorial_string = str(total_n).replace(",", "*")
+    n_fatorial_string = "".join(n_fatorial_string)
+    n_fatorial = functools.reduce(operator.mul, total_n)
 
     total_k = list(range(1, k + 1))
 
-    k_fat = str(total_k).replace(",", "*")
-    k_fat = "".join(k_fat)
-    k = functools.reduce(operator.mul, total_k)
+    k_fatorial_string = str(total_k).replace(",", "*")
+    k_fatorial_string = "".join(k_fatorial_string)
+    k_fatorial = functools.reduce(operator.mul, total_k)
 
-    comb = n / (k * (proddif))
+    resultado = n_fatorial / (k_fatorial * (dif_produto))
 
     print("\nResolução:\n")
 
-    print(f"\n\nC(n,k)= {nn}!/({kk}!*({nn}-{kk})!")
+    print(f"\nC(n,k)= (n+1)!/(k!*(n-k)!")
+    print(f"\nC(n,k)= {n}!/({k}!*({n}-{k})!)")
+    print(
+        f"\n{n_fatorial_string} / ({k_fatorial_string}*{dif_fatorial})")
+
+    print(f"\nC(n,k)= {n_fatorial}/({k_fatorial}*({dif_produto})) = {resultado}")
 
     print(
-        f"\n{n_fat}\n______________________________________________________________________________________________\n({k_fat}*{dif_fat})")
+        f"\n\n Existem {resultado} maneiras possiveis de se organizar esses livros")
 
-    print(f"\n\nC(n,k)= {n}/({k}*({proddif})) = {comb}")
-
-    print(
-        f"\n\n Existem {comb} maneiras possiveis de se organizar esses livros")
-
-    cont = True
-    while (cont):
+    novaQuestao = True
+    while novaQuestao:
         resp = input("\n\nDeseja criar outra questão? (Y/N) \n")
 
         x = resp.upper()
 
         if x == "Y":
-            cont = False
+            novaQuestao = False
         elif x == "N":
-            d = False
-            cont = False
+            continuar = False
+            novaQuestao = False
         else:
             print("\n\n***Você precisa escolher Y(sim) ou N(não)***\n\n")
             continue
